d2fe31b67de9baac7c5e9d0693327d0b7b7fb14e
sawurov <rsnsavursky@icloud.com>
2021-06-28 14:19:08 +0300

    first deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Table extends Component{
+  render() {
+    return(
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Name
+            </th>
+            <th>
+              Job
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Charlie
+            </td>
+            <td>
+              Janitor
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Mac
+            </td>
+            <td>
+              Bouncer
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Dee
+            </td>
+            <td>
+              Aspring actress
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Charlie
+            </td>
+            <td>
+              Janitor
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Charlie
+            </td>
+            <td>
+              Janitor
+            </td>
+          </tr>
+          
+        </tbody>
+      </table>
+    )
+  }
 }
 
 export default App;
